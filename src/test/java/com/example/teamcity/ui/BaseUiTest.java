f9abccfd17ae53143242cd7a0d3116058d317857
Valentina <valentina.misem@gmail.com>
2025-11-26 16:10:56 +0100

    BaseUITest

--- src/test/java/com/example/teamcity/ui/BaseUiTest.java
+++ src/test/java/com/example/teamcity/ui/BaseUiTest.java
@@ -1,4 +1,31 @@
 package com.example.teamcity.ui;
 
-public class BaseUiTest {
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.example.teamcity.BaseTest;
+import com.example.teamcity.api.config.Config;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeSuite;
+
+import java.util.Map;
+
+public class BaseUiTest extends BaseTest {
+    @BeforeSuite(alwaysRun = true)
+    public void setupUiTest() {
+        Configuration.browser = Config.getProperty("browser");
+        Configuration.baseUrl = "http://" + Config.getProperty("host");
+        // не пишите UI тесты с локальным браузером
+        // а потом вы запускаете на remote browser
+        Configuration.remote = Config.getProperty("remote"); // тестовый эндпоинт для запуска тестов
+        Configuration.browserSize = Config.getProperty("browserSize");
+
+        Configuration.browserCapabilities.setCapability("selenoid:options",
+                Map.of("enableVNC", "true",
+                        "enableLog", "true"));
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void closeWebDriver() {
+        Selenide.closeWebDriver();
+    }
 }
