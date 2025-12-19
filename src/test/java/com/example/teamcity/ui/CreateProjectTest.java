6a3070dbb0b5512e5811e5f6a188e2228b8ff4d7
Valentina <valentina.misem@gmail.com>
2025-12-19 22:48:48 +0100

    Allure

--- src/test/java/com/example/teamcity/ui/CreateProjectTest.java
+++ src/test/java/com/example/teamcity/ui/CreateProjectTest.java
@@ -13,7 +13,7 @@ import static com.codeborne.selenide.Selenide.page;
 
 @Test(groups = {"Regression"})
 public class CreateProjectTest extends BaseUiTest {
-    private static final String GIT_URL = "https://github.com/valemisem/mapal-testing";
+    private static final String GIT_URL = "https://github.com/valemisem/nobugs-java";
 
     @Test(description = "User should be able to create project", groups = {"Positive"})
     public void userCreatesProject() {
